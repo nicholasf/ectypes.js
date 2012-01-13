7b96f5d1b5aaa7bd447db17d706788cbd819ca3c
Nicholas Faiz <nicholas.faiz@gmail.com>
2012-01-13 13:49:52 +1100

    deps in package.json

--- mill.js
+++ mill.js
@@ -1,4 +1,13 @@
 exports.mill =  function() {
-	//locate the mills.js file
-	
+	var machines;
+
+	//1. locate the mills.js file
+	//2. look for any pre-existing state in mill.machines
+
+	//3. make it work?
+
+	//
+	function machines(){
+		this.machines = {}; //...		
+	}
 }
