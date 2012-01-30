ba5876621338fdf447be6b8ed4ceb688b910aed4
Nicholas Faiz <nicholas.faiz@gmail.com>
2012-01-30 13:53:19 +1100

    correcting paths for requiring drafts and mongoose modules in tests

--- test/strategies/mongoose.js
+++ test/strategies/mongoose.js
@@ -1,29 +1,31 @@
 var Faker = require('faker');
 var should = require('should');
-var drafts = require('./../drafts');
+var drafts = require('./../../drafts');
 
+//configure Mongoose
 var mongoose = require('mongoose')
   , Schema = mongoose.Schema;
 
-var toySchema = new Schema({
+var ToySchema = new Schema({
 	name: String
 });
 
-var toyBoxSchema = new Schema({
+var ToyBoxSchema = new Schema({
     name: String,
     toys: [ToySchema] 
 });
 
 
-var db = mongoose.createConnection('mongodb://host/db')
+var db = mongoose.createConnection('mongodb://localhost/draftsjs-test')
 
-db.model('Toy', toySchema);
-db.model('ToyBox', toyBoxSchema);
+db.model('Toy', ToySchema);
+db.model('ToyBox', ToyBoxSchema);
 
 
-var mongooseStrategy = require('./strategies/mongoose');
+var mongooseStrategy = require('./../../lib/strategies/mongoose');
 mongooseStrategy.setDB(db)
 drafts.setDefaultStrategy(mongooseStrategy);
+
 
 it('creates a simple model', function(){
 
@@ -37,5 +39,4 @@ it('creates a simple model', function(){
 	toybox = drafts.ToyBox();
 
 	should.exist(toybox);
-//	toybox.name.should.be.a('string');
 });
