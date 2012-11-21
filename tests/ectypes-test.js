d630c82ff8f303f5249aec4de8e17fa2ae50e54b
Nicholas Faiz <nicholas.faiz@gmail.com>
2012-11-22 07:21:14 +1100

    befores being processed in ectypes

--- tests/ectypes-test.js
+++ tests/ectypes-test.js
@@ -30,69 +30,69 @@ describe('configuring strategies', function(){
   });
 });
 
-// describe('creating producers from blueprints', function(){
+describe('creating producers from blueprints', function(){
 
-//   it('creates a producer for a single blueprinted Project', function(){
-//     ctx.load(new SimpleStrategy());
-//     ctx.add(projectBlueprint);
-//     should.exist(ctx.Project);
-//   }); 
+  it('creates a producer for a single blueprinted Project', function(){
+    ctx.load(new SimpleStrategy());
+    ctx.add(projectBlueprint);
+    should.exist(ctx.Project);
+  }); 
 
-//   it('creates producers for an array of blueprints', function(){
-//     ctx.load(new SimpleStrategy());
-//     ctx.add(multiBlueprint);
-//     should.exist(ctx.Person);
-//     should.exist(ctx.Ancestor);
-//   });
+  it('creates producers for an array of blueprints', function(){
+    ctx.load(new SimpleStrategy());
+    ctx.add(multiBlueprint);
+    should.exist(ctx.Person);
+    should.exist(ctx.Ancestor);
+  });
 
-//   it('maps the strategies functions onto the producer', function(){
-//     ctx.load(new SimpleStrategy());
-//     ctx.add(projectBlueprint);
-//     should.exist(ctx.Project.build);
-//   });
+  it('maps the strategies functions onto the producer', function(){
+    ctx.load(new SimpleStrategy());
+    ctx.add(projectBlueprint);
+    should.exist(ctx.Project.build);
+  });
 
-//   it('the build function on simpleStrategy called from the producer works', function(){
-//     ctx.load(new SimpleStrategy());
-//     ctx.add(projectBlueprint);
+  it('the build function on simpleStrategy called from the producer works', function(){
+    ctx.load(new SimpleStrategy());
+    ctx.add(projectBlueprint);
 
-//     var cb = function(err, project){
-//       should.exist(project.title);
-//     }
+    var cb = function(err, project){
+      should.exist(project.title);
+    }
 
-//     ctx.Project.build(cb);
-//   });
+    ctx.Project.build(cb);
+  });
 
-  // it('does not create ectype data before the producer function is called', function(){
-  //   ctx.load(new SimpleStrategy());
-  //   ctx.add(projectBlueprint);
-  //   should.exist(ctx.Project);
-  // });
-// });
+  it('does not create ectype data before the producer function is called', function(){
+    ctx.load(new SimpleStrategy());
+    ctx.add(projectBlueprint);
+    should.exist(ctx.Project);
+  });
+});
 
 
-// describe('overriding values', function(){
-//   ctx.load(new SimpleStrategy());
-//   ctx.add(projectBlueprint);
+describe('overriding values', function(){
+  ctx.load(new SimpleStrategy());
+  ctx.add(projectBlueprint);
 
-//   it('without getting them confused with the cb', function(){
-//       var overrider = {title: 'was overridden'};
-//       var cb = function(err, project){
-//         project.title.should.equal('was overridden');       
-//       }
+  it('without getting them confused with the cb', function(){
+      var overrider = {title: 'was overridden'};
+      var cb = function(err, project){
+        project.title.should.equal('was overridden');       
+      }
 
-//       ctx.Project.build(overrider, cb);
-//     });
+      ctx.Project.build(overrider, cb);
+    });
   
-//     it('includes override fields which don\'t exist in the blueprint', function(){
-//       var overrider = {unblueprinted: 'should appear', title: 'was still overrdidden'};
-//       var cb = function(err, project){
-//         project.title.should.equal('was still overrdidden');
-//         should.exist(project.unblueprinted)
-//       }
+    it('includes override fields which don\'t exist in the blueprint', function(){
+      var overrider = {unblueprinted: 'should appear', title: 'was still overrdidden'};
+      var cb = function(err, project){
+        project.title.should.equal('was still overrdidden');
+        should.exist(project.unblueprinted)
+      }
 
-//       ctx.Project.build(overrider, cb);
-//     });
-// });
+      ctx.Project.build(overrider, cb);
+    });
+});
 
 
 var projectDependencyBlueprint = {
@@ -115,7 +115,6 @@ describe('dependencies', function(done){
     ctx.add(projectDependencyBlueprint);
 
     ctx.Project.build( function(err, project){
-      console.log('huh?')
       project.third_id.should.equal(100)
       done()
     });
